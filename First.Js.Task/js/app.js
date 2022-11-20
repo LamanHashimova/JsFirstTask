7d4eb8faaf721ab9340762afe81a6690bf7805b8
LamanHashimova <112489993+LamanHashimova@users.noreply.github.com>
2022-11-21 02:23:24 +0400

    Add files via upload

---
+++ First.Js.Task/js/app.js
@@ -0,0 +1,23 @@
+//Task-1
+
+// var CurrentTime = new Date().getHours();
+// if(CurrentTime>=6 && CurrentTime<12){
+//     alert('Sabahiniz xeyir');
+//     document.body.style.backgroundImage="url('/assets/photos/morning.jpg')"
+//     document.body.style.backgroundSize='cover';
+// }else if(CurrentTime>=12 && CurrentTime<18){
+//     alert('Gunortaniz xeyir');
+//     document.body.style.backgroundImage="url('/assets/photos/evening.jpg')"
+//     document.body.style.backgroundSize='cover';
+// }else(CurrentTime>=18 && CurrentTime<6); {
+//     alert('Geceniz  xeyir');
+//     document.body.style.backgroundImage="url('/assets/photos/night.jpg')"
+//     document.body.style.backgroundSize='cover';
+// }
+
+Task-2
+
+var radius = 5;
+var S=Math.PI * Math.pow(radius,2);
+console.log(S)
+
